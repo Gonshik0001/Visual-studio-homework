975b03f672f420f353311390ad9cbb4ae21b4450
Gonshik0001 <kulshaev@yandex.ru>
2024-01-17 00:54:59 -0500

    Homework 2.1.4 made the name short

--- Homework 2.1.3/task_score.js
+++ Homework 2.1.3/task_score.js
@@ -5,7 +5,8 @@ let total = intro + git + js
 let average = total/3
 let studentName = "Ivan"
 let studentLastName = "Sidorov"
+let shortStudentName = studentName.slice(0, 1);
 console.log (total)
 console.log (average)
-console.log (`Total score of ${studentName} ${studentLastName} is ${total}`)
-console.log (`Average score of ${studentName} ${studentLastName} is ${average}`)
+console.log (`Total score of ${studentName} is ${total}`)
+console.log (`Average score of ${shortStudentName}. ${studentLastName} is ${average}`)
