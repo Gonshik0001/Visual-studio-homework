20a2ffeb5c3dd54d192e1231838c7bc13c17b9da
Gonshik0001 <kulshaev@yandex.ru>
2024-01-17 00:08:43 -0500

    Added new file Homework 2.1.3

---
+++ Homework 2.1.3/task_score.js
@@ -0,0 +1,11 @@
+let intro = 100
+let git = 200
+let js = 300
+let total = intro + git + js 
+let average = total/3
+let studentName = "Ivan"
+let studentLastName = "Sidorov"
+console.log (total)
+console.log (average)
+console.log (`Total score of ${studentName} ${studentLastName} is ${total}`)
+console.log (`Average score of ${studentName} ${studentLastName} is ${average}`)
