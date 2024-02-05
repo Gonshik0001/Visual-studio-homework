29cc67fe43ef31d139b5b93b5a2c21029bc2c1f6
Gonshik0001 <kulshaev@yandex.ru>
2024-02-04 23:31:25 -0500

    Homework 2.4.3 added file script.js

---
+++ Homework 2.4.3/script.js
@@ -0,0 +1,9 @@
+const calculateBonus = (a, b) => {
+    let bonus;
+    const sum = a + b;
+    debugger; //посчитана только сумма. Но еще не подсчитано будет ли бонус
+    sum > 50 ? (bonus = 50) : (bonus = sum);
+    debugger; //проверка будет ли предоставлен бонус (если сумма больше 50)
+    return bonus;
+    };
+    calculateBonus (28,35);
