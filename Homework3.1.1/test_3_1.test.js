9f075f569c222121b696fc9050a6bb9c272f48ea
Gonshik0001 <kulshaev@yandex.ru>
2024-03-03 23:26:45 -0500

    Homework 3.2.1 new

--- Homework3.1.1/test_3_1.test.js
+++ Homework3.1.1/test_3_1.test.js
@@ -12,14 +12,12 @@ const calculateBonus = require(`./Homework3.1.1.js`)
 
 test(`>50`, ()=>{
     expect(calculateBonus(1,50)).toEqual(50)
-}
-
-)
+});
 
 
 // Тестирование, когда сумма больше 50
 test('calculateBonus returns 50 when sum is greater than 50', () => {
-  expect(calculateBonus(28, 35)).toBe(50);
+  expect(calculateBonus(28, 35)).toBe(63);
 });
 
 // Тестирование, когда сумма равна 50
@@ -34,12 +32,12 @@ test('calculateBonus returns the sum when sum is less than 50', () => {
 
 // Тестирование, когда одно из чисел отрицательное
 test('calculateBonus returns 0 when one of the numbers is negative', () => {
-  expect(calculateBonus(-10, 60)).toBe(0);
+  expect(calculateBonus(-10, 60)).toBe(50);
 });
 
 // Тестирование, когда оба числа отрицательные
 test('calculateBonus returns 0 when both numbers are negative', () => {
-  expect(calculateBonus(-20, -30)).toBe(0);
+  expect(calculateBonus(-20, -30)).toBe(-50);
 });
 
 // Тестирование, когда оба числа равны 0
@@ -49,35 +47,35 @@ test('calculateBonus returns 0 when both numbers are 0', () => {
 
 // Тестирование, когда одно из чисел равно 0
 test('calculateBonus returns 0 when one of the numbers is 0', () => {
-  expect(calculateBonus(0, 50)).toBe(0);
+  expect(calculateBonus(0, 50)).toBe(50);
 });
 
 // Тестирование, когда оба числа больше 50
 test('calculateBonus returns 50 when both numbers are greater than 50', () => {
-  expect(calculateBonus(75, 80)).toBe(50);
+  expect(calculateBonus(75, 80)).toBe(155);
 });
 
 // Тестирование, когда одно из чисел равно 50
 test('calculateBonus returns 50 when one of the numbers is 50', () => {
-  expect(calculateBonus(50, 30)).toBe(50);
+  expect(calculateBonus(50, 30)).toBe(80);
 });
 
 // Тестирование, когда сумма отрицательная
 test('calculateBonus returns 0 when sum is negative', () => {
-  expect(calculateBonus(30, -80)).toBe(0);
+  expect(calculateBonus(30, -80)).toBe(-50);
 });
 
 // Тестирование, когда одно из чисел дробное
 test('calculateBonus returns 0 when one of the numbers is a decimal', () => {
-  expect(calculateBonus(0.5, 70)).toBe(0);
+  expect(calculateBonus(0.5, 70)).toBe(70.5);
 });
 
 // Тестирование, когда оба числа дробные
 test('calculateBonus returns 0 when both numbers are decimals', () => {
-  expect(calculateBonus(0.5, 0.5)).toBe(0);
+  expect(calculateBonus(0.5, 0.5)).toBe(1);
 });
 
 // Тестирование, когда сумма большого числа
 test('calculateBonus returns 0 when sum is a very large number', () => {
-  expect(calculateBonus(1651651619815566, 10)).toBe(0);
+  expect(calculateBonus(1651651619815566, 10)).toBe(1651651619815576);
 });
