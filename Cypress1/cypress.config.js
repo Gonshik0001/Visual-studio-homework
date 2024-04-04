1d53fcebd92c936d42f72c7b27b491a7f2c72dc8
Gonshik0001 <kulshaev@yandex.ru>
2024-04-04 00:55:03 -0400

    HW521

--- Cypress1/cypress.config.js
+++ Cypress1/cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  projectId: "vpg586"
+   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
---
+++ Cypress1/cypress/e2e/3-SQLVeriier/firstCy.js
@@ -0,0 +1,5 @@
+describe('First tests suit', ()=>{
+it('First test', ()=>{
+    Cy.visit("https://example.cypress.io/todo");
+    })
+})
