fcc36ddffb90adf87882438d4cb0df4c95e030b9
Gonshik0001 <kulshaev@yandex.ru>
2024-04-15 01:20:13 -0400

    HW 5.2.2 edited

--- Cypress1/cypress/support/headersCheck.cy.js
+++ Cypress1/cypress/support/headersCheck.cy.js
@@ -1,64 +1,58 @@
-describe('Проверка ссылок в header меню для зарегистрированного пользователя (студента)', () => {
-    beforeEach(() => {
-      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/login');
-      cy.get('input[name="username"]').type('student');
-      cy.get('input[name="password"]').type('123456!', { log: false });
-      cy.contains('Sign In').click();
-      cy.wait(1000); // Добавим задержку для обработки входа
-    });
-  
-    it('Ссылка "Home" работает', () => {
-      cy.contains('Home').click();
-      cy.url().should('include', '/home');
-    });
-  
-    it('Ссылка "Profile" работает', () => {
-      cy.contains('Profile').click();
-      cy.url().should('include', '/profile');
-    });
-  
-    it('Ссылка "Browse Exercises" работает', () => {
-      cy.contains('Browse Exercises').click();
-      cy.url().should('include', '/exercises');
-    });
-  
-    it('Ссылка "My Exercises" работает', () => {
-      cy.contains('My Exercises').click();
-      cy.url().should('include', '/myexercises');
-    });
-  
-    it('Ссылка "My Results" работает', () => {
-      cy.contains('My Results').click();
-      cy.url().should('include', '/myresults');
-    });
-  
-    it('Ссылка "Leaderboard" работает', () => {
-      cy.contains('Leaderboard').click();
-      cy.url().should('include', '/leaderboard');
-    });
-  
-    it('Ссылка "Challenges" работает', () => {
-      cy.contains('Challenges').click();
-      cy.url().should('include', '/challenges');
-    });
-  
-    it('Ссылка "Messages" работает', () => {
-      cy.contains('Messages').click();
-      cy.url().should('include', '/messages');
-    });
-  
-    it('Ссылка "Settings" работает', () => {
-      cy.contains('Settings').click();
-      cy.url().should('include', '/settings');
-    });
-  
-    it('Ссылка "Sign Out" работает', () => {
-      cy.contains('Sign Out').click();
-      cy.url().should('include', '/login');
-    });
-  
-    it('Ссылка "Help" работает', () => {
-      cy.contains('Help').click();
-      cy.url().should('include', '/help');
-    });
+describe('User Menu Links Test', () => {
+  beforeEach(() => {
+    // Логин пользователя
+    cy.login('student', '123456!');
   });
+
+  it('Check "Home" link', () => {
+    cy.get('.navbar-nav').contains('Home').click();
+    cy.url().should('include', '/home');
+    cy.get('h1').should('contain', 'Home'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Courses" link', () => {
+    cy.get('.navbar-nav').contains('Courses').click();
+    cy.url().should('include', '/courses');
+    cy.get('h1').should('contain', 'Courses'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Profile" link', () => {
+    cy.get('.navbar-nav').contains('Profile').click();
+    cy.url().should('include', '/profile');
+    cy.get('h1').should('contain', 'Profile'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Grades" link', () => {
+    cy.get('.navbar-nav').contains('Grades').click();
+    cy.url().should('include', '/grades');
+    cy.get('h1').should('contain', 'Grades'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Messages" link', () => {
+    cy.get('.navbar-nav').contains('Messages').click();
+    cy.url().should('include', '/messages');
+    cy.get('h1').should('contain', 'Messages'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Settings" link', () => {
+    cy.get('.navbar-nav').contains('Settings').click();
+    cy.url().should('include', '/settings');
+    cy.get('h1').should('contain', 'Settings'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Help" link', () => {
+    cy.get('.navbar-nav').contains('Help').click();
+    cy.url().should('include', '/help');
+    cy.get('h1').should('contain', 'Help'); // Проверка правописания названия страницы
+  });
+
+  it('Check "About" link', () => {
+    cy.get('.navbar-nav').contains('About').click();
+    cy.url().should('include', '/about');
+    cy.get('h1').should('contain', 'About'); // Проверка правописания названия страницы
+  });
+
+  it('Check "Contact" link', () => {
+    cy.get('.navbar-nav').contains('Contact').click();
+    cy.url().should('include', '/contact');
+    cy.g
