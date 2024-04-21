80f8220055aa862b72947730a44c6c11ba414b71
Gonshik0001 <kulshaev@yandex.ru>
2024-04-21 10:18:26 -0400

    Homework 5.2.4

---
+++ Cypress1/cypress/e2e/3-SQLVeriier/verifiertest2.cy.js
@@ -0,0 +1,7 @@
+describe('Verifier Page Load Test', () => {
+    it('successfully loads Verifier page', () => {
+      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/');
+      cy.get('.nav-link.active').should('exist');
+    });
+  });
+  
